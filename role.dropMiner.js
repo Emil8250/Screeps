6b1e636a383f5e9f1f8401b8600f8922349d2825
Emil Skov Olesen <emil8250@ymail.com>
2020-08-16 20:11:10 +0200

    debug

--- role.dropMiner.js
+++ role.dropMiner.js
@@ -8,10 +8,9 @@ var roleDropminer = {
                         return (structure.structureType == STRUCTURE_CONTAINER);
                     }
             });
-            var containerKeys = Object.keys(creep.room.creeps);
-            for (var i = 0; i < containerKeys.length; i++) {
-              var currentCreep = creep.room.creeps[containerKeys[i]];
-              console.log(currentCreep);
+            var dropMiners = var harvesters2 = _.filter(Game.creeps, (creep) => creep.memory.role == 'dropMiner');
+            for (var i = 0; i < dropMiners.length; i++) {
+              console.log(dropMiners[i]);
             }
             if(creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
                 creep.moveTo(targets[0], {visualizePathStyle: {stroke: '#ffaa00'}});
