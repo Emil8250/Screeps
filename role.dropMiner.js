711d9c8715940759b1c271ca39f4132df5bf91e6
Emil Skov Olesen <emil8250@ymail.com>
2020-08-16 22:46:16 +0200

    Update role.dropMiner.js

--- role.dropMiner.js
+++ role.dropMiner.js
@@ -22,7 +22,7 @@ var roleDropminer = {
             }
            console.log(targets[1]);
 
-if(creep.memory.container == "")
+if(creep.memory.container2 == "")
 {
                 if(!takenContainers.includes(targets[0].id)){
                   creep.memory.container2 = targets[0].id;
