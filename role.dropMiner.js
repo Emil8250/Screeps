17178442502264a7eae2bfdfc7a325fc995b0494
Emil Skov Olesen <emil8250@ymail.com>
2020-08-16 19:54:39 +0200

    dropMiner

--- role.dropMiner.js
+++ role.dropMiner.js
@@ -1,37 +1,28 @@
-var roleHarvester = {
+var roleDropminer = {
 
     /** @param {Creep} creep **/
     run: function(creep) {
-        creep.say(creep.store.getUsedCapacity())
-	    if(creep.store.getUsedCapacity() < creep.store.getCapacity()) {
             var sources = creep.room.find(FIND_SOURCES);
-            if(creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
-                creep.moveTo(sources[0], {visualizePathStyle: {stroke: '#ffaa00'}});
-            }
-        }
-        else {
             var targets = creep.room.find(FIND_STRUCTURES, {
                     filter: (structure) => {
-                        return (structure.structureType == STRUCTURE_EXTENSION || structure.structureType == STRUCTURE_SPAWN) &&
-                            structure.store.getFreeCapacity(RESOURCE_ENERGY) > 0;
+                        return (structure.structureType == STRUCTURE_CONTAINER);
                     }
             });
+            var containerKeys = Object.keys(creep.room.creeps);
+            for (var i = 0; i < containerKeys.length; i++) {
+              var currentCreep = creep.room.creeps[containerKeys[i]];
+              console.log(currentCreep.id);
+            }
+            if(creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
+                creep.moveTo(targets[0], {visualizePathStyle: {stroke: '#ffaa00'}});
+            }
+
             if(targets.length > 0 && targets[0].energy < targets[0].energyCapacity) {
                 if(creep.transfer(targets[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
                     creep.moveTo(targets[0], {visualizePathStyle: {stroke: '#ffffff'}});
                 }
             }
-            else{
-               // creep.moveTo(31,31, {visualizePathStyle: {stroke: '#ffffff'}});
-               	        var targets = creep.room.find(FIND_CONSTRUCTION_SITES);
-            if(targets.length) {
-                if(creep.build(targets[0]) == ERR_NOT_IN_RANGE) {
-                    creep.moveTo(targets[0], {visualizePathStyle: {stroke: '#ffffff'}});
-                }
-            }
-        }
-        }
 	}
 };
 
-module.exports = roleHarvester;
+module.exports = roleDropminer;
