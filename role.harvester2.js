1fd31074321c1601f96a5a857a11ee03002d7c3a
Emil Skov Olesen <emil8250@ymail.com>
2020-08-16 16:49:07 +0200

    Fixed harvester2.

--- role.harvester2.js
+++ role.harvester2.js
@@ -2,12 +2,28 @@ var roleHarvester2 = {
 
     /** @param {Creep} creep **/
     run: function(creep) {
-        creep.say(creep.store.getUsedCapacity())
 	    if(creep.store.getUsedCapacity() < creep.store.getCapacity()) {
-            var sources = creep.room.find(FIND_SOURCES);
-            if(creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
+          var sources = creep.room.find(FIND_STRUCTURES, {
+                  filter: (structure) => {
+                      return (structure.structureType == STRUCTURE_CONTAINER);
+                  }
+          });
+          var backup = creep.room.find(FIND_STRUCTURES, {
+                  filter: (structure) => {
+                      return (structure.structureType == STRUCTURE_STORAGE);
+                  }
+          });
+          if(sources[0].store.getUsedCapacity() > 150)
+          {
+            if(creep.withdraw(sources[0]) == ERR_NOT_IN_RANGE) {
                 creep.moveTo(sources[0], {visualizePathStyle: {stroke: '#ffaa00'}});
             }
+          }
+          else {
+            if(creep.withdraw(backup[0]) == ERR_NOT_IN_RANGE) {
+                creep.moveTo(backup[0], {visualizePathStyle: {stroke: '#ffaa00'}});
+            }
+          }
         }
         else {
             var targets = creep.room.find(FIND_STRUCTURES, {
@@ -16,20 +32,9 @@ var roleHarvester2 = {
                             structure.store.getFreeCapacity(RESOURCE_ENERGY) > 0;
                     }
             });
-            if(targets.length > 0 && targets[0].energy < targets[0].energyCapacity) {
                 if(creep.transfer(targets[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
                     creep.moveTo(targets[0], {visualizePathStyle: {stroke: '#ffffff'}});
                 }
-            }
-            else{
-               // creep.moveTo(31,31, {visualizePathStyle: {stroke: '#ffffff'}});
-               	        var targets = creep.room.find(FIND_CONSTRUCTION_SITES);
-            if(targets.length) {
-                if(creep.build(targets[0]) == ERR_NOT_IN_RANGE) {
-                    creep.moveTo(targets[0], {visualizePathStyle: {stroke: '#ffffff'}});
-                }
-            }
-        }
         }
 	}
 };
