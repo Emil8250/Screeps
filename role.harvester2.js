d6efae9f54f9aa06781db8fff3c604b3f0168757
Emil Skov Olesen <emil8250@ymail.com>
2020-08-16 20:47:59 +0200

    debug

--- role.harvester2.js
+++ role.harvester2.js
@@ -18,12 +18,12 @@ var roleHarvester2 = {
           if(sources[0].store.getUsedCapacity() > 150)
           {
             creep.say(creep.withdraw(sources[0]));
-            if(creep.withdraw(sources[0]) == ERR_NOT_IN_RANGE) {
+            if(creep.withdraw(sources[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
                 creep.moveTo(sources[0], {visualizePathStyle: {stroke: '#ffaa00'}});
             }
           }
           else {
-            if(creep.withdraw(backup[0]) == ERR_NOT_IN_RANGE) {
+            if(creep.withdraw(backup[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
                 creep.moveTo(backup[0], {visualizePathStyle: {stroke: '#ffaa00'}});
             }
           }
