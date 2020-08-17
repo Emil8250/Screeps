2e7a863c13e680f0537b3146ee1514fb2c56a7ea
Emil Skov Olesen <emil8250@ymail.com>
2020-08-17 08:22:30 +0200

    Update role.hauler.js

--- role.hauler.js
+++ role.hauler.js
@@ -22,7 +22,7 @@ var roleHauler = {
             }
         }
 	creep.say(creep.store.getUsedCapacity());
-        if(creep.store.getUsedCapacity > 0)
+        if(creep.store.getUsedCapacity() > 0)
         {
             if(creep.transfer(currentStorage, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE){
                 creep.moveTo(currentStorage);
