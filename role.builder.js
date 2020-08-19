5a178653c92ae76b45e07a19bfbe087f6db65619
Emil Skov Olesen <emil8250@ymail.com>
2020-08-19 21:48:29 +0200

    bugfix

--- role.builder.js
+++ role.builder.js
@@ -14,7 +14,9 @@ var roleBuilder = {
 
 	    if(creep.memory.building) {
 	        var targets = creep.room.find(FIND_CONSTRUCTION_SITES);
+
             if(targets.length) {
+              creep.say(creep.build(targets[0]);
                 if(creep.build(targets[0]) == ERR_NOT_IN_RANGE) {
                     creep.moveTo(targets[0], {visualizePathStyle: {stroke: '#ffffff'}});
                 }
