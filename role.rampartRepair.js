60519db34cc2cc957a55c1b27e4d83e54d8fa624
Emil Skov Olesen <emil8250@ymail.com>
2020-08-20 07:06:21 +0200

    Update role.rampartRepair.js

--- role.rampartRepair.js
+++ role.rampartRepair.js
@@ -14,7 +14,7 @@ var roleRampartRepair = {
 
         var lowRampart = _.min(targets, function(rampart) { return rampart.hits; });
         var currentStorage = _.max(sources, function(source) { return source.store.getUsedCapacity(RESOURCE_ENERGY); });
-        creep.memory.rampart = lowRampart.id;
+      //  creep.memory.rampart = lowRampart.id;
         if(!creep.memory.rampart){
             creep.memory.rampart = lowRampart.id;
         }
