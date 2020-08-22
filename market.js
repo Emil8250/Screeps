f43be97373c5c0333e7c911203db6ccb80e5a845
Emil Skov Olesen <emil8250@ymail.com>
2020-08-22 22:15:33 +0200

    fix

--- market.js
+++ market.js
@@ -5,6 +5,9 @@ var market = {
     run: function() {
       var test = Game.market.getAllOrders();
       var orders = _.filter(test, function(order) {return order.resourceType == PIXELS && order.type == ORDER_BUY});
-      _.max(orders, function(order){return order.price}).price;
+      console.log('best price & fee: ' + _.max(orders, function(order){return order.price}).price);
+      var roomName = _.max(orders, function(order){return order.price}).roomName;
+      var fee = Game.market.calcTransactionCost(1, roomName, Game.spawns.Spawn1.room);
+      console.log(fee);
 };
 module.exports = market;
