857b3f7f97c2b92855f343bbfa8c784ac4a3583a
Emil Skov Olesen <emil8250@ymail.com>
2020-08-24 09:27:52 +0200

    Update market.js

--- market.js
+++ market.js
@@ -1,14 +1,18 @@
 var market = {
 
     run: function() {
-      var test = Game.market.getAllOrders();
-      var orders = _.filter(test, function(order) {return order.resourceType == PIXEL && order.type == ORDER_BUY});
-      var maxOrder =  _.max(orders, function(order){return order.price});
-      console.log('best price & fee: ' + _.max(orders, function(order){return order.price}).price);
-      console.log(maxOrder.id);
-      console.log(maxOrder.amount);
-      if(maxOrder.price > 745 && Game.resources['pixel'] > 0){
-        Game.market.deal(maxOrder.id, Game.resources['pixel']);
+      if(Memory.nextMarketCheck > Game.time)
+      {
+          var test = Game.market.getAllOrders();
+          var orders = _.filter(test, function(order) {return order.resourceType == PIXEL && order.type == ORDER_BUY});
+          var maxOrder =  _.max(orders, function(order){return order.price});
+          console.log('best price & fee: ' + _.max(orders, function(order){return order.price}).price);
+          console.log(maxOrder.id);
+          console.log(maxOrder.amount);
+          if(maxOrder.price > 745 && Game.resources['pixel'] > 0){
+            Game.market.deal(maxOrder.id, Game.resources['pixel']);
+          }
+          Memory.nextMarketCheck = Game.time + 1200;
       }
     }
 };
