924f104919e88866b68fb9e2365f3022c5de942e
Emil Skov Olesen <emil8250@ymail.com>
2020-08-26 15:09:14 +0200

    Update main.js

--- main.js
+++ main.js
@@ -230,7 +230,7 @@ module.exports.loop = function () {
         currentSpawn.spawnCreep([WORK,CARRY,CARRY,WORK,MOVE,MOVE], newName,
             {memory: {role: 'supplyTower'}});
     }
-    else if(currentRoom.energyAvailable >= 900 && upgraders2.length < 7) {
+    else if(currentRoom.energyAvailable >= 900 && upgraders2.length < 6) {
         var newName = 'Upgrader2' + Game.time;
         console.log('Spawning new upgrader: ' + newName);
         currentSpawn.spawnCreep([WORK,WORK,WORK,WORK,WORK,CARRY,CARRY,CARRY,CARRY,CARRY,CARRY,CARRY,MOVE,MOVE,MOVE], newName,
